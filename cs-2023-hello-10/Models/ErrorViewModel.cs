3afcb6da10f52700d04f463f22a4438fee64e86b
callor <callor@callor.com>
2023-03-09 10:37:40 +0900

    dotnet-start

---
+++ cs-2023-hello-10/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cs_2023_hello_10.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
